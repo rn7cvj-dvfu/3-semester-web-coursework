be3942f414b30127bea3f88756b78445f99e751d
Vyacheslav <slavyanka345@mail.ru>
2024-02-01 09:03:59 +1000

    fix

--- android/app/src/main/kotlin/com/example/web_susch/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.web_susch
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-const double appPaddingSmall = 8.0;
-const double appPaddingMedium = 18.0;
-const double appPaddingLarge = 26.0;
-
-const double appRoundRadiusSmall = 8.0;
-const double appRoundRadiusMedium = 18.0;
-const double appRoundRadiusLarge = 26.0;
